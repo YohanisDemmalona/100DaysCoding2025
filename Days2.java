ebed07322454526a5524242a1974074524a985a5
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-09 15:11:01 +0800

    Create Days2.java

---
+++ Days2.java
@@ -0,0 +1,10 @@
+package day2coding100;
+public class Day2Coding100 {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+       System.out.println("World");  
+        System.out.println("Day 2");
+         System.out.printf("Nama: %s, Umur: %d, NIM: %s", "Yohanis Demmalona", 19, "D0225314");
+
+      }
+}
