c6169c8feffc421b99722abe4defa4c88a4f3691
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-10 07:27:04 +0800

    Create Days3.java

---
+++ Days3.java
@@ -0,0 +1,11 @@
+package day3coding;
+public class Day3Coding {
+    public static void main(String[] args) {
+        // Contoh komentar untuk satu baris
+       
+    /*    
+    ini adalah contoh komentar multi baris.
+    komentar ini untuk memberikan penjelasan yang lebih panjang
+    */
+    }  
+}
