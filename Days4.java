c0d712e06bf73fb920d698034e01ef2bd957399a
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-11 09:29:59 +0800

    Create Days4.java

---
+++ Days4.java
@@ -0,0 +1,18 @@
+public class Day4Coding {
+    public static void main(String[] args) {
+        /*
+        contoh Escape Sequence
+        New line (\n) Baris Baru
+        */        
+        System.out.println("Hello\nWorld!");
+        // Tab (\t) Tab
+        System.out.println("Nama\tYohanis Demmalona");
+        System.out.println("Umur\t19");
+        // Backslash (\\) 
+        System.out.println("C:\\Users\\ADVAN\\Documents\\NetBeansProjects");
+         // Double quote (\") Kutip ganda
+        System.out.println("Dia berkata, \"Hello World!\"");
+        // Single quote (\') Kutip tunggal
+        System.out.println("Saya kuliah di hari Jum'at");
+    }   
+}
