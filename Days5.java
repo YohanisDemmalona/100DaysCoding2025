7a805035f8575e57245108bd060f56bc80da0bb3
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-12 10:43:55 +0800

    Create Days5.java

---
+++ Days5.java
@@ -0,0 +1,13 @@
+package day5coding;
+public class Day5Coding {
+    public static void main(String[] args) {
+       byte nilaiSatu = 100;
+        short nilaiDua = 20000;
+        int nilaiTiga = 1000000;
+        long nilaiEmpat = 1000000000L;
+        System.out.println("Nilai byte: " + nilaiSatu);
+        System.out.println("Nilai short: " + nilaiDua);
+        System.out.println("Nilai int: " + nilaiTiga);
+        System.out.println("Nilai long: " + nilaiEmpat);
+    } 
+}
