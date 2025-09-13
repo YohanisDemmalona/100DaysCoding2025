25ed5281d4b8fdee5ade3a7de1f98e5ee80e33a6
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-13 18:59:19 +0800

    Create Days6.java

---
+++ Days6.java
@@ -0,0 +1,15 @@
+package day6coding;
+public class Day6Coding {
+    public static void main(String[] args) {
+        /*
+        float 6-7 digit desimal (f jika ada angka di blkng koma)
+        double 15-16 digit desimal
+        */
+      float nilaiFloat = 2.2210f;
+        double nilaiDouble = 3.123456789;
+        
+        System.out.println("Nilai float: " + nilaiFloat);
+        System.out.println("Nilai double: " + nilaiDouble);
+
+    }    
+}
