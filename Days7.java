dbffb952ad977f6496b5c40015964a9863114880
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-14 16:31:17 +0800

    Create Days7.java

---
+++ Days7.java
@@ -0,0 +1,11 @@
+package day7coding;
+public class Day7Coding {
+    public static void main(String[] args) {
+        char saudara = '4';
+        boolean salah = false;
+        boolean benar = true;
+        System.out.println("saudara : " + saudara );
+        System.out.println("salah : " + salah);
+        System.out.println("benar : " + benar);;
+    }
+}
