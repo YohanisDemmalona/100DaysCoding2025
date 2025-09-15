2719a001207c164bed9985b90fd18d4c1f2d4d21
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-15 13:55:34 +0800

    Create Days8.java

---
+++ Days8.java
@@ -0,0 +1,7 @@
+package day8coding;
+public class Day8Coding {
+    public static void main(String[] args) {
+        String Nama = "Yohanis";
+        System.out.println("Nama : " + Nama);
+    }  
+}
