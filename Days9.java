ff372c9ad16915006106d77de1d6701c929f163f
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-16 15:12:11 +0800

    Create Days9.java

---
+++ Days9.java
@@ -0,0 +1,19 @@
+package day9coding;
+public class Day9Coding {
+    public static void main(String[] args) {
+        //Deklarasi
+        int Umur;
+        String nama;
+        double beratBadan;
+        nama = "Anis";
+        Umur = 19;
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + Umur);
+        //Update varialble
+        int angka = 5;
+        System.out.println("Nilai sebelum update : " + angka);
+        
+        angka = 10;
+        System.out.println("Nilai setelah update : " + angka);
+    }
+}
