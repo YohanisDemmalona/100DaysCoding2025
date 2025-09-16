78302956e58344a6f3ce55efa1cd232a83848b3c
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-16 15:31:45 +0800

    Update Days9.java

--- Days9.java
+++ Days9.java
@@ -2,13 +2,15 @@ package day9coding;
 public class Day9Coding {
     public static void main(String[] args) {
         //Deklarasi
-        int Umur;
         String nama;
-        double beratBadan;
+        int Umur;
+        double Beratbadan;
         nama = "Anis";
         Umur = 19;
+        Beratbadan = 40.5
         System.out.println("Nama : " + nama);
         System.out.println("Umur : " + Umur);
+        System.out.println("Berat Badan : " + Beratbadan + "kg");
         //Update varialble
         int angka = 5;
         System.out.println("Nilai sebelum update : " + angka);
