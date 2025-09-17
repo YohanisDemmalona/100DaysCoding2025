2a80569fb2e83e7468046490623994914038df96
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-17 10:05:06 +0800

    Create Days10.java

---
+++ Days10.java
@@ -0,0 +1,10 @@
+package day10coding;
+public class Day10Coding {
+    public static void main(String[] args) { 
+       //nilai Constanta tidak bisa di ubah
+       final int MAX = 100;
+       final double PHI_LINGKARAN = 3.14;
+       System.out.println("Nilai MAX = " + MAX);
+       System.out.println("PHI = " + PHI_LINGKARAN);
+    } 
+}
