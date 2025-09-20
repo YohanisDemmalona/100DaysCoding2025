440c618b96df94908bc6183903d35779664c13be
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-20 18:27:59 +0800

    Create Days13.java

---
+++ Days13.java
@@ -0,0 +1,12 @@
+package day13coding;
+public class Day13coding {
+    public static void main(String[] args) {
+       final String Nama = "Yohanis";
+       final String Nim = "D0225314";
+       final String tanggalLahir ="22 Oktober 2005";
+       
+        System.out.println("Nama: "+Nama);
+        System.out.println("NIM: "+Nim);
+        System.out.println("Tanggal Lahir: "+tanggalLahir);
+    }
+}
