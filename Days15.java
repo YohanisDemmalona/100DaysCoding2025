31186c76887a095c167033bb1300d8d04d7520e7
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-22 19:01:25 +0800

    Create Days15.java

---
+++ Days15.java
@@ -0,0 +1,15 @@
+package day15coding;
+public class Day15Coding {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int hasil = a+b;
+        System.out.println("Hasil 5+4 = "+hasil);
+        
+        int c = 5;
+        int d = 4;
+        int jumlah = c-d;
+        System.out.println("Hasil 5-4 = "+jumlah);
+   
+    } 
+}
