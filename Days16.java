5637e61353a206dfe7b49502054e61f690def335
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-23 19:52:04 +0800

    Create Days16.java

---
+++ Days16.java
@@ -0,0 +1,12 @@
+package day16coding;
+public class Day16Coding {
+    public static void main(String[] args) {
+        int  x = 10;
+        int Y = 2;
+        int hasil = x*Y;
+        int hsil = x/Y;
+        System.out.println("hasil kali: "+hasil);
+        System.out.println("hasil bagi: "+hsil);
+          
+    } 
+}
