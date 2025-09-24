8b4f58c30865f900ed9a9ddec1d5b25a5440aad8
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-24 19:14:21 +0800

    Create Days17.java

---
+++ Days17.java
@@ -0,0 +1,9 @@
+package day17coding;
+public class Day17Coding {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int sisa = x%y;
+        System.out.println("Sisa Bagi: "+sisa);
+    }
+}
