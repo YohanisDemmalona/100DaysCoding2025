6608da351e0d04d7142bba6c6012d705c2f85d1f
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-25 20:00:46 +0800

    Create Days18.java

---
+++ Days18.java
@@ -0,0 +1,17 @@
+package day18coding;
+public class Day18Coding {
+public static void main(String[] args) {
+        byte nilaiByte = 22;
+        short nilaiShort = nilaiByte;
+        int nilaiInt = nilaiShort;
+        long nilaiLong = nilaiInt;
+        float nilaifloat = nilaiLong;
+        double nilaiDouble = nilaifloat;
+        System.out.println(nilaiByte);
+        System.out.println(nilaiShort);
+        System.out.println(nilaiInt);
+        System.out.println(nilaiLong);
+        System.out.println(nilaifloat);
+        System.out.println(nilaiDouble);
+    }  
+}
