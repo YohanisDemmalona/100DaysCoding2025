20ff689ccba4fec48ef3378dc544912325cff774
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-09-27 19:24:25 +0800

    Create Days20.java

---
+++ Days20.java
@@ -0,0 +1,39 @@
+package day20coding;
+public class Day20Coding {
+
+    public static void main(String[] args) {
+        
+        byte b = 10;
+        String s = String.valueOf(b);
+        System.out.println(s);
+
+        short c = 10;
+        String tr = String.valueOf(c);
+        System.out.println(tr);
+        
+        int d = 10;
+        String e = String.valueOf(d);
+        System.out.println(e);
+        
+        long q =10000L;
+        String w = String.valueOf(q);
+        System.out.println(w);
+        
+        float p =10f;
+        String r = String.valueOf(p);
+        System.out.println(r);
+        
+        double t =10;
+        String y =String.valueOf(t);
+        System.out.println(y);
+        
+        boolean x = true;
+        String z = String.valueOf(x);
+        System.out.println(z);
+        
+        char g = 'A';
+        String v = String.valueOf(g);
+        System.out.println(y);
+                
+    }  
+}
