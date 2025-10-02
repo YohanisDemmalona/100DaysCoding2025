ba910c59ade278e9481f5b4d7dc91c95d5294a1b
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-02 21:14:25 +0800

    Create Days25.java

---
+++ Days25.java
@@ -0,0 +1,12 @@
+package day25coding;
+public class Day25Coding {
+    public static void main(String[] args) {
+        int x = 7; 
+       // Increment
+        int a = ++x;
+       // Decrement
+        int b = --x;  
+        System.out.println("a: "+a);
+        System.out.println("b: "+b);
+    }  
+}
