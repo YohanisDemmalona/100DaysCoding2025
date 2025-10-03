1086ad53d0da6993c8599f9f80d6c34fabd4c434
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-03 17:55:07 +0800

    Create Days26.java

---
+++ Days26.java
@@ -0,0 +1,17 @@
+package day26coding;
+public class Day26Coding {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("Nilai x = "+x);
+        x += 5;
+        System.out.println("Nilai x += "+x);
+        x -= 5;
+        System.out.println("Nilai x -= "+x);
+        x *= 5;
+        System.out.println("Nilai x *= "+x);
+        x /= 5;
+        System.out.println("Nilai x /= "+x);
+        x %= 5;
+        System.out.println("Nilai x %= "+x);
+    }
+}
