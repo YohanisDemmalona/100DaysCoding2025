9ef83dee0312e4fe06a905f1db227c31898eb2c5
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-05 21:39:18 +0800

    Create Days28.java

---
+++ Days28.java
@@ -0,0 +1,9 @@
+package day28coding;
+public class Day28Coding {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 5;
+        System.out.println(x == y);
+        System.out.println(x != y);
+    } 
+}
