f8d8dfc45d138a0bda653c198df730349e0081c5
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-06 16:58:29 +0800

    Create Days29.java

---
+++ Days29.java
@@ -0,0 +1,9 @@
+package day29coding;
+public class Day29Coding {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4;
+        System.out.println(x < y);
+        System.out.println(x > y);
+    }  
+}
