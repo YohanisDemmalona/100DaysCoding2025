4dd7eefca29d3dc4c89321aba5c90392c8b2ab6f
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-07 21:28:04 +0800

    Create Days30.java

---
+++ Days30.java
@@ -0,0 +1,9 @@
+package day30coding;
+public class Day30Coding {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+    }  
+}
