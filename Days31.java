fdad952d1b6140461c76d3bffa29f8048f02e9d5
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-08 19:52:28 +0800

    Create Days31.java

---
+++ Days31.java
@@ -0,0 +1,13 @@
+package day30coding;
+public class day31Coding {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        if (x > 0 && y > 5) {
+            System.out.println("Keduanya benar");
+        } else {
+            System.out.println("Salah satu atau keduanya salah");
+        }
+    }   
+}
