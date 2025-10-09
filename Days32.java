ce06a723f24c18041c05f2dd3ea2af1d68aaf0f1
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-09 21:33:20 +0800

    Create Days32.java

---
+++ Days32.java
@@ -0,0 +1,13 @@
+package day32coding;
+public class day32Coding {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        if (x > 0 || y < 5) {
+            System.out.println("Salah satu kondisi benar");
+        } else {
+            System.out.println("Kedua kondisi salah");
+        }  
+    }
+}
