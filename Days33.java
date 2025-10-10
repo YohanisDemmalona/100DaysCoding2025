718f7177164957ccdf41584fd2efd9d399dbdb82
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-10 21:59:50 +0800

    Create Days33.java

---
+++ Days33.java
@@ -0,0 +1,8 @@
+package day33coding;
+public class day33Coding {
+    public static void main(String[] args) {
+        boolean Admin = true;
+        boolean NotAdmin = !Admin;
+        System.out.println(NotAdmin);
+    }
+}
