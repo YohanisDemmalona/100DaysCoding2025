3fa14e6df617a5edc68b5c1c8a9f31ef321d2598
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-12 19:23:46 +0800

    Update Days35.java

--- Days35.java
+++ Days35.java
@@ -1,14 +1,15 @@
 package day30coding;
-import java.util.Scanner;
 public class day35Coding {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);     
-        int nilai = sc.nextInt();
-        int kehadiran = sc.nextInt();
-        if(nilai >=60 && kehadiran >=80){
-            System.out.println("siswa dapat mengikuti ujian akhir semester");
-        }else{
-            System.out.println("siswa tidak dapat mengikuti ujian akhir semester");
-        }
+        
+    int nilai = 80;
+        
+    if (nilai >= 80) {
+        System.out.println("Nilai Anda A");
+    } else if (nilai >= 60) {
+        System.out.println("Nilai Anda B");
+    } else {
+        System.out.println("Nilai Anda C");
+    }
     }
 }
