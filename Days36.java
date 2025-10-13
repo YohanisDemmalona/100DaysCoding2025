33faf5dbba5a4211e6d88f1ba4cfcfce798dea7e
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-13 22:43:16 +0800

    Create Days36.java

---
+++ Days36.java
@@ -0,0 +1,27 @@
+package day30coding;
+import java.util.Scanner;
+public class day36Coding {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Hari: ");
+        int hari = sc.nextInt();
+        
+        if(hari<=5){
+           if(hari%2==0){
+               System.out.println("kecil dari lima dan genap");
+               if(hari==2){
+                   System.out.println("dua,kecil dari lima dan genap");
+               }else{
+                   System.out.println("bukan dua,kecil dari lima dan genap");
+               }
+           }else if  (hari%2==1){
+               System.out.println("kecil dari lima dan ganjil");
+           }
+        }else if(hari>5)
+            if(hari%2==0){
+               System.out.println("kecil dari lima dan genap");
+           }else if  (hari%2==1){
+               System.out.println("kecil dari lima dan ganjil");
+           }
+    }
+}
