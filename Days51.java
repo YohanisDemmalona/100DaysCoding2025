b837bdfeec7959c263b5343b831a0d0e9967c887
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-28 20:16:42 +0800

    Create Days51.java

---
+++ Days51.java
@@ -0,0 +1,9 @@
+package day50coding;
+public class day51Coding {
+    public static void main(String[] args) {  
+      
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
