71d498d7c3c32658aa844af8a046bd8f7f4723dd
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-10-29 22:44:40 +0800

    Create Days52.java

---
+++ Days52.java
@@ -0,0 +1,10 @@
+package day50coding;
+public class day52Coding {
+    public static void main(String[] args) {
+        int a =1;
+        while(a<=5){
+           System.out.println("Angka "+a);
+           a++;
+       }
+    } 
+}
