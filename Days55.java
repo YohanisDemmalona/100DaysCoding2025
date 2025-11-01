56495d0319484298ce8d79ace3dd1c959baede0c
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-01 20:51:42 +0800

    Create Days55.java

---
+++ Days55.java
@@ -0,0 +1,14 @@
+package day50coding;
+import java.util.Scanner;
+public class day55Coding {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nilai: ");
+        int nilai = sc.nextInt();
+        if(nilai>70){
+            System.out.println("Ujian Kroco Ji Pale");
+        }else{
+            System.out.println("Sa Jappo'ka Aih");
+        }
+    }
+}
