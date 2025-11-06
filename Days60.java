c032f4571c76b3c6392435bb5553fbd8b3b21c8e
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-06 20:41:05 +0800

    Create Days60.java

---
+++ Days60.java
@@ -0,0 +1,24 @@
+package day50coding;
+import java.util.Scanner;
+public class day60Coding {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.print("Masukkan Nilai N: ");
+        int N = sc.nextInt();
+        
+        System.out.println("Angka ganjil N hingga 1:");
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("\nAngka genap N hingga 1:");
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
