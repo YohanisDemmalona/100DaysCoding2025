55c96e9121d4fe514587c63409564ddc16e57df6
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-12 20:11:19 +0800

    Create Days66.java

---
+++ Days66.java
@@ -0,0 +1,12 @@
+package day50coding;
+import java.util.Scanner;
+public class day66Coding {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan NIlai N: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+                System.out.println("*"); 
+        }
+    }
+}
