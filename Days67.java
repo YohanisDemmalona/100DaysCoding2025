afda218d4536bbd3133bba62a45164fd3782f213
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-13 16:27:36 +0800

    Create Days67.java

---
+++ Days67.java
@@ -0,0 +1,12 @@
+package day50coding;
+import java.util.Scanner;
+public class day67Coding {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Nilai N: ");
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.print("*");
+        }System.out.println();
+    }
+}
