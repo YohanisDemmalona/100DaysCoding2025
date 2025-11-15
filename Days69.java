7358d8a6b5e935d9190d43ae450b7f92280d800f
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-15 19:22:46 +0800

    Create Days69.java

---
+++ Days69.java
@@ -0,0 +1,10 @@
+package ddp5;
+import java.util.Scanner;
+public class NewClass1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jam = sc.nextInt();
+        jam *= 60;
+        System.out.println(jam);   
+    }
+}
