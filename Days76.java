ee3924745512c1311cfe97faab6469f6f58bec6f
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-22 19:09:23 +0800

    Create Days76.java

---
+++ Days76.java
@@ -0,0 +1,11 @@
+package day70coding;
+public class day76Coding {
+    public static void main(String[] args) {    
+        String nama = "Anis";
+        System.out.println(nama.equals("Anis")); // true
+        System.out.println(nama.equalsIgnoreCase("ANIS")); // true
+        System.out.println(nama.contains("ANis")); // true
+        System.out.println(nama.isEmpty()); // false
+        System.out.println("".isEmpty()); // true
+    }
+}
