b48cfbf659dc31be5f3b71a1ca5465128e0f5781
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-23 18:42:25 +0800

    Create Days77.java

---
+++ Days77.java
@@ -0,0 +1,16 @@
+package day70coding;
+public class day77Coding {
+    public static void main(String[] args) {
+        String nama = " Yohanis ";
+
+        // substring() - mengambil substring dari string
+        System.out.println(nama.substring(1, 5));
+
+        // replace() - mengganti karakter atau substring dalam string
+        System.out.println(nama.replace(" ", "_")); 
+
+        // trim() - menghapus whitespace di awal dan akhir string (spasi)
+        System.out.println(nama.trim()); 
+ 
+    }
+}
