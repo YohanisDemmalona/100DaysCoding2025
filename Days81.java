fc391292ac5c4efc2b29f4272cedc9672b9dca97
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-11-27 17:32:19 +0800

    Create Days81.java

---
+++ Days81.java
@@ -0,0 +1,11 @@
+package day70coding;
+public class day81Coding {
+    public static void main(String[] args) {
+       int [] angka = {1, 2, 3, 4, 5};
+        System.out.println(angka[0]);
+        System.out.println(angka[1]);
+        System.out.println(angka[2]);
+        System.out.println(angka[3]);
+        System.out.println(angka[4]);
+    }
+}
