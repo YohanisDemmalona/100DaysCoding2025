9e6158c1f2c2682e22b40461bc4e4229415d3568
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-12-08 21:11:30 +0800

    Create Days92.java

---
+++ Days92.java
@@ -0,0 +1,10 @@
+package day70coding;
+public class day92Coding {
+    public static void main(String[] args) {
+        int hasil = tambah(5, 4);
+        System.out.println(hasil);
+    }
+    static int tambah(int a, int b){
+       return a + b; //mengembalikan nilai dari method.
+    }
+}
