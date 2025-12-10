1ce80fd2ee734488faec96093433f9c2e7ee50bb
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-12-10 21:14:37 +0800

    Create Days94.java

---
+++ Days94.java
@@ -0,0 +1,13 @@
+package day70coding;
+public class day94Coding {
+    public static void main(String[] args) {
+        cetakNama("Yohanis Demmalona'");
+        cetakUmur(20);
+    }
+    static void cetakNama(String nama){
+        System.out.println("Nama Saya : " + nama); 
+    }
+    static void cetakUmur(int umur){
+        System.out.println("Umur Saya : " + umur);
+    }
+}
