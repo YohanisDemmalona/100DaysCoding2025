8a790e70b11f77f41ebaa5262b49c14c36ad1a8a
YohanisDemmalona <yohanisdemmalona22@gmail.com>
2025-12-13 20:49:15 +0800

    Create Days97.java

---
+++ Days97.java
@@ -0,0 +1,9 @@
+package day70coding;
+public class day97Coding {
+    public static void main(String[] args) {
+        System.out.println(hitungLuas(2,5));  
+    }
+    static int hitungLuas(int panjang, int lebar){
+        return panjang * lebar;
+    }
+}
